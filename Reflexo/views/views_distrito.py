6f686a060276a19929572b9ad41f67f885f183e7
RuizMiguel-A <ruizmigueldev@gmail.com>
2025-08-08 12:46:49 -0500

    comrprobando ci

--- Reflexo/views/views_distrito.py
+++ Reflexo/views/views_distrito.py
@@ -1,54 +1,11 @@
-from django.db import models
+# Este archivo debería contener vistas, no modelos
+# Los modelos ya están definidos en Reflexo/models/
 
-class Province(models.Model):
-    name = models.CharField(max_length=255)
+from django.shortcuts import render
+from django.http import JsonResponse
+from Reflexo.models import District
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        app_label = 'Reflexo'
-
-
-class District(models.Model):
-    name = models.CharField(max_length=255)
-    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='districts')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        app_label = 'Reflexo'
-
-
-class DistrictUser(models.Model):
-    name = models.CharField(max_length=255)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='users')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        app_label = 'Reflexo'
-
-
-class DistrictPatient(models.Model):
-    name = models.CharField(max_length=255)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='patients')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        app_label = 'Reflexo'
-
-
-class DistrictTherapist(models.Model):
-    name = models.CharField(max_length=255)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='therapists')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        app_label = 'Reflexo'
+def list_districts(request):
+    """Vista para listar distritos"""
+    districts = District.objects.values('id', 'name', 'province__name')
+    return JsonResponse(list(districts), safe=False)
