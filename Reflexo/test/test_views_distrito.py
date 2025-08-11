d8a4fd67ab6b6e9be6af245a566bd877c4296e73
César Martel <alex.martel.castillo@gmail>
2025-08-11 03:25:17 -0500

    Añadido test para la vista DistrictView

--- Reflexo/test/test_views_distrito.py
+++ Reflexo/test/test_views_distrito.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from Reflexo.models import District, Province, Region
+import json
 
 class DistrictModelTest(TestCase):
     def setUp(self):
@@ -13,3 +15,18 @@ class DistrictModelTest(TestCase):
 
     def test_str_method(self):
         self.assertEqual(str(self.district), "Miraflores")
+
+class DistrictViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.region = Region.objects.create(name="Costa")
+        self.province = Province.objects.create(name="Lima", region=self.region)
+        self.district = District.objects.create(name="Miraflores", province=self.province)
+
+    def test_district_view(self):
+        response = self.client.get('/api/districts/')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertTrue(len(data) > 0)
+        self.assertEqual(data[0]['name'], 'Miraflores')
+        self.assertEqual(data[0]['province__name'], 'Lima')
